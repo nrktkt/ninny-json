3095d565ac203bc4e5504aa074c6f254fff078e8
Nathan Fischer <nathan@nrktkt.tk>
2022-01-21 15:22:51 -0800

    use the publish function in the modules

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,7 @@ echo $SIGNING_KEY | base64 --decode > signing.key
 gpg --import signing.key
 shred signing.key
 
-PUBLISH_VERSION=$TRAVIS_TAG ./mill -i --disable-ticker mill.scalalib.PublishModule/publishAll \
+PUBLISH_VERSION=$TRAVIS_TAG ./mill -i --disable-ticker __.publish \
 	--sonatypeCreds $SONATYPE_NAME:$SONATYPE_PW \
 	--gpgArgs --passphrase=$GPG_PW,--batch,--yes,-a,-b \
 	--publishArtifacts __.publishArtifacts \
