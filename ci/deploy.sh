8a2ffa643ffd0c2ff8272b4dfc4001417f26b4e9
Nathan Fischer <nathan@nrktkt.tk>
2022-01-21 15:37:45 -0800

    or not

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,9 @@ echo $SIGNING_KEY | base64 --decode > signing.key
 gpg --import signing.key
 shred signing.key
 
-PUBLISH_VERSION=$TRAVIS_TAG ./mill -i --disable-ticker __.publish \
+PUBLISH_VERSION=$TRAVIS_TAG ./mill -i --disable-ticker mill.scalalib.PublishModule/publishAll \
+	--sonatypeUri 'https://s01.oss.sonatype.org/service/local' \
+	--sonatypeSnapshotUri 'https://s01.oss.sonatype.org/content/repositories/snapshots' \
 	--sonatypeCreds $SONATYPE_NAME:$SONATYPE_PW \
 	--gpgArgs --passphrase=$GPG_PW,--batch,--yes,-a,-b \
 	--publishArtifacts __.publishArtifacts \
