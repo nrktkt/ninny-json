ff32c3cd99d4cae58a2f8db1385274fa330cb1b5
Nathan Fischer <nath@nfischer.io>
2023-09-28 14:03:46 -0700

    Improved ToJson[Either] instances

--- ninny/test/src/nrktkt/ninny/ToJsonInstancesSpec.scala
+++ ninny/test/src/nrktkt/ninny/ToJsonInstancesSpec.scala
@@ -6,20 +6,37 @@ import org.scalatest.matchers._
 import nrktkt.ninny.ast._
 
 class ToJsonInstancesSpec extends AnyFlatSpec with should.Matchers {
-  "Either values" should 
+  "Either values" should
     "convert right to json" in {
-      val right = Right[Int, String]("test")
+      val right                       = Right[Int, String]("test")
       val either: Either[Int, String] = right
-      val expected = Some(JsonString("test"))
+      val expected                    = Some(JsonString("test"))
       right.toJson shouldEqual expected
       either.toJson shouldEqual expected
     }
 
-    it should "convert left to json" in {
-      val left = Left[Int, String](5)
+  it should "convert left to json" in {
+    val left                        = Left[Int, String](5)
+    val either: Either[Int, String] = left
+    val expected                    = Some(JsonNumber(5))
+    left.toJson shouldEqual expected
+    either.toJson shouldEqual expected
+  }
+
+  it should "provide ToSomeJson when available" in {
+    {
+      val left                        = Left[Int, String](5)
       val either: Either[Int, String] = left
-      val expected = Some(JsonNumber(5))
-      left.toJson shouldEqual expected
-      either.toJson shouldEqual expected
+      val expected                    = JsonNumber(5)
+      left.toSomeJson shouldEqual expected
+      either.toSomeJson shouldEqual expected
     }
+    {
+      val right                       = Right[Int, String]("test")
+      val either: Either[Int, String] = right
+      val expected                    = JsonString("test")
+      right.toSomeJson shouldEqual expected
+      either.toSomeJson shouldEqual expected
+    }
+  }
 }
